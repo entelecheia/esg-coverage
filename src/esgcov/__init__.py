00a4e85d5e5dea50fd8123a6431c3aff2f39a3b5
Young Joon Lee <entelecheia@hotmail.com>
2023-06-27 06:33:02 +0900

    fix(config): update global configuration path in init file

--- src/esgcov/__init__.py
+++ src/esgcov/__init__.py
@@ -9,6 +9,8 @@ __package_name__ = os.path.basename(os.path.dirname(__file__))
 
 # Extract package information
 about.__package_name__ = __package_name__
+global_config.hyfi_config_module = about.config_module
+global_config.hyfi_config_path = about.config_path
 
 
 def get_version() -> str:
